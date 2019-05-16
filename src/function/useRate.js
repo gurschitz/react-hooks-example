dd03bbb7d6a817695d2981eb7f85b1b4875fd7ce
Gerald Urschitz <gerald.urschitz@gmail.com>
2019-05-16 15:20:09 +0200

    fix import

--- src/function/useRate.js
+++ src/function/useRate.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import getRate from "./getRate";
+import getRate from "../getRate";
 
 function useRate(baseCurrency, currency) {
   const delay = 5000;
